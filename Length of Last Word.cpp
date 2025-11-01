f2a564bce94f9eabd744c2e145c265ec64a6327d
PhamHuy <phamvanhuy1709@gmail.com>
2025-11-01 17:01:22 +0700

    upload

---
+++ Length of Last Word.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int lengthOfLastWord(string s) {
+    int i = s.size() - 1;
+    cout << "Input string size: " << s.size() << endl;
+    int length = 0;
+
+    // Bỏ khoảng trắng ở cuối
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    // Đếm độ dài của từ cuối cùng
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+}
+
+int main() {
+    string s = "   fly me   to   the moon  ";
+    cout << lengthOfLastWord(s);  // Output: 4
+}
