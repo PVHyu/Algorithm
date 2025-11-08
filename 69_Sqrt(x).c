9466d4efae6c4bd154176730aeb097e3e4532213
PhamHuy <phamvanhuy1709@gmail.com>
2025-11-08 13:55:20 +0700

    upload ex69 of LeetCode

---
+++ 69_Sqrt(x).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int mySqrt(int x) {
+    if (x < 2)
+        return x;
+
+    long left = 1, right = x / 2, ans = 0;
+    while (left <= right) {
+        long mid = left + (right - left) / 2;
+        long sq = mid * mid;
+
+        if (sq == x)
+            return mid;
+        else if (sq < x) {
+            ans = mid;
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int x;
+    printf("Nhap x: ");
+    scanf("%d", &x);
+    printf("Can bac hai (phan nguyen) cua %d la: %d\n", x, mySqrt(x));
+    return 0;
+}
